f7d321e6150f3385b695f9da575f213918055161
ahmedzarzamony <42036857+ahmedzarzamony@users.noreply.github.com>
2021-05-30 13:03:32 +0200

    Create image-viewer.php

---
+++ image-viewer.php
@@ -0,0 +1,14 @@
+Route::get('images/{filename}', function ($filename)
+{
+    $path = storage_path() . '/img/' . $filename;
+
+    if(!File::exists($path)) abort(404);
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
